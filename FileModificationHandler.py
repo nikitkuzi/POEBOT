d55e34a5d1bed2390910bf3eb18fd1387f9eb0a7
nikitkuzi <nikitkuzi@gmail.com>
2023-01-04 18:55:25 +0300

    initial commit

---
+++ FileModificationHandler.py
@@ -0,0 +1,20 @@
+import os, time, traceback
+
+class FileModified():
+    def __int__(self, file_path, callback):
+        self.file_path = file_path
+        self.callback = callback
+        self.modified = os.path.getatime(file_path)
+
+    def start(self):
+        try:
+            while True:
+                time.sleep(0.5)
+                modified = os.path.getmtime(self.file_path)
+                if modified!= self.modified:
+                    self.modified = modified
+                    if self.callback():
+                        break
+        except Exception as e:
+            print(traceback.format_exc())
+
