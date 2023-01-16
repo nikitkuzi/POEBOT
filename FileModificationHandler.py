f474278b6bd29c59ed559d7113021d81a819774b
nikitkuzi <nikitkuzi@gmail.com>
2023-01-16 12:55:06 +0300

    typo

--- FileModificationHandler.py
+++ FileModificationHandler.py
@@ -4,15 +4,15 @@ class FileModified():
     def __init__(self, file_path, callback):
         self.file_path = file_path
         self.callback = callback
-        self.modified = os.path.getatime(file_path)
+        self.last_time_modified = os.path.getmtime(self.file_path)
 
     def start(self):
         try:
             while True:
                 time.sleep(0.5)
-                modified = os.path.getmtime(self.file_path)
-                if modified!= self.modified:
-                    self.modified = modified
+                new_modified = os.path.getmtime(self.file_path)
+                if new_modified != self.last_time_modified:
+                    self.last_time_modified = new_modified
                     if self.callback():
                         break
         except Exception as e:
